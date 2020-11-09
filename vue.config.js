a8ed4160deeeb1039b8f89939116b28f5a5e3139
Dhruv Bhanushali <dhruv_b@live.com>
2020-11-09 22:17:05 +0530

    Drop the options UI

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   pages: {
-    options: {
-      template: 'public/browser-extension.html',
-      entry: './src/options/main.js',
-      title: 'Serenite - Options'
-    },
     override: {
       template: 'public/browser-extension.html',
       entry: './src/override/main.js',
