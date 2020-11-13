faba275bd9a396bbb5843c3a8a93a00516a46e60
Dhruv Bhanushali <dhruv_b@live.com>
2020-11-13 20:27:18 +0530

    Define the header component

---
+++ src/atoms/serene-header/SereneHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <header class="serene-header">
+    <h1 class="heading">{{ heading }}</h1>
+    <p class="subheading">{{ subheading }}</p>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'SereneHeader',
+    props: {
+      /**
+       * the heading for the particular page
+       *
+       * This is displayed in big, bold and prominently-colored type and is the
+       * focus of the page.
+       */
+      heading: {
+        type: String,
+        required: true
+      },
+      /**
+       * the subheading for the particular page
+       *
+       * This is displayed in subdued-colored type and provides not-so-important
+       * information.
+       */
+      subheading: {
+        type: String,
+        default: ''
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss" src="./SereneHeader.scss"></style>
