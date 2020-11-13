a8ecbb62846e8ada4e07742df2d93a1153fa5047
Dhruv Bhanushali <dhruv_b@live.com>
2020-11-13 20:22:07 +0530

    Define the atoms of the user interface

---
+++ src/atoms/serene-icon/SereneIcon.vue
@@ -0,0 +1,47 @@
+<template>
+  <svg
+    class="icon"
+    viewBox="0 0 512 512">
+    <use :href="href"/>
+  </svg>
+</template>
+
+<script>
+  export default {
+    name: 'SereneIcon',
+    props: {
+      /**
+       * the name of the icon
+       *
+       * This is the ID of the group to be used from the SVG.
+       */
+      name: {
+        type: String,
+        required: true
+      },
+      /**
+       * the path to the icon
+       *
+       * This is the URL as obtained from the webpack loaders with the use of
+       * `import` or `require` statements.
+       */
+      path: {
+        type: String,
+        required: true
+      }
+    },
+    computed: {
+      /**
+       * Get the hyperlink to the group ID to be used from the SVG. It is
+       * expressed as a string of the form `path#id`.
+       *
+       * @return {string} the path to the SVG group to be used in the icon
+       */
+      href () {
+        return `${this.path}#${this.name}`
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss" src="./SereneIcon.scss"/>
---
+++ src/atoms/serene-input/SereneInput.vue
@@ -0,0 +1,81 @@
+<template>
+  <label class="serene-input" :class="sereneInputClasses">
+    <input
+      v-bind="$attrs"
+      v-on="$listeners"
+      class="field"
+      type="text"
+      :value="text"
+      @input="handleInput"/>
+    <span class="label">
+      <!-- @slot Icon goes here -->
+      <slot name="icon"/>
+    </span>
+    <span
+      class="enter"
+      title="Press Enter to submit.">
+      <Icon
+        name="enter"
+        :path="icons.enter"/>
+    </span>
+  </label>
+</template>
+
+<script>
+  import Icon from '@/atoms/serene-icon/SereneIcon'
+
+  import enter from '@/assets/icons/enter.svg'
+
+  export default {
+    name: 'SereneInput',
+    components: { Icon },
+    inheritAttrs: false,
+    model: {
+      prop: 'text',
+      event: 'update'
+    },
+    data () {
+      return {
+        icons: {
+          enter
+        }
+      }
+    },
+    props: {
+      /**
+       * the text to use as the content of the input field
+       */
+      text: {
+        type: String,
+        required: true
+      }
+    },
+    computed: {
+      /**
+       * Get the classes to apply on the ActionField instance.
+       *
+       * @return {Array} an array of classes to apply on the element
+       */
+      sereneInputClasses () {
+        return [
+          {
+            'is-filled': Boolean(this.text)
+          }
+        ]
+      }
+    },
+    methods: {
+      /**
+       * Emit an `'update'` event with the value of the target input field as
+       * the payload.
+       *
+       * @param {InputEvent} event - the event that triggered this handler
+       */
+      handleInput (event) {
+        this.$emit('update', event.target.value)
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss" src="./SereneInput.scss"/>
