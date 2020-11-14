ec20c164a46df4e15bc17c080519e3208ba6a9ae
Dhruv Bhanushali <dhruv_b@live.com>
2020-11-14 09:47:55 +0530

    Define a minimal button component

---
+++ src/atoms/serene-button/SereneButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button
+    v-on="$listeners"
+    class="serene-button">
+    <!-- @slot Content goes here -->
+    <slot/>
+  </button>
+</template>
+
+<script>
+  export default {
+    name: 'SereneButton'
+  }
+</script>
+
+<style scoped lang="scss" src="./SereneButton.scss"/>
