e481e98d0e9f061813c0ccddf3ad06169b4fa7f6
Dhruv Bhanushali <dhruv_b@live.com>
2020-11-14 13:08:52 +0530

    Create the header components for the task and label pages

---
+++ src/components/labels/status/Status.vue
@@ -0,0 +1,47 @@
+<template>
+  <SereneHeader
+    heading="Labels"
+    :subheading="labelMessage"/>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+
+  import SereneHeader from '@/atoms/serene-header/SereneHeader'
+
+  export default {
+    name: 'Status',
+    components: {
+      SereneHeader
+    },
+    computed: {
+      /**
+       * Get the count of all currently defined labels.
+       *
+       * @return {number} the number of labels defined
+       */
+      labelCount () {
+        return this.allLabels.length
+      },
+      /**
+       * Get a message describing the count of the currently described labels,
+       * while also handling pluralisation.
+       *
+       * @return {string} a message describing the number of labels defined
+       */
+      labelMessage () {
+        if (this.labelCount === 0) {
+          return 'No labels'
+        } else if (this.labelCount === 1) {
+          return '1 label'
+        } else {
+          return `${this.labelCount} labels`
+        }
+      },
+
+      ...mapGetters('todo', [
+        'allLabels'
+      ])
+    }
+  }
+</script>
---
+++ src/components/tasks/today/Today.vue
@@ -0,0 +1,109 @@
+<template>
+  <SereneHeader
+    :heading="day"
+    :subheading="date"/>
+</template>
+
+<script>
+  import SereneHeader from '@/atoms/serene-header/SereneHeader'
+
+  export default {
+    name: 'Today',
+    components: {
+      SereneHeader
+    },
+    data () {
+      return {
+        now: {
+          dayOfWeek: null,
+
+          date: null,
+          month: null,
+          year: null
+        },
+
+        looper: null,
+        interval: 5 // minutes
+      }
+    },
+    computed: {
+      /**
+       * Get the current day of the week. The first day of the week is taken to
+       * be Sunday.
+       *
+       * @return {string} the name of the current day of the week
+       */
+      day () {
+        const DAYS_OF_WEEK = [
+          'Sunday',
+          'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', // Workdays
+          'Saturday'
+        ]
+        return DAYS_OF_WEEK[this.now.dayOfWeek]
+      },
+      /**
+       * Get the current date in the format `dd mmmm, yyyy` according to the
+       * Wikipedia time coding format. This is equivalent to the Python
+       * `strftime` format `%d %B, %Y`.
+       *
+       * @return {string} the date in a readable format
+       * @see {@link https://strftime.org|Python `strftime` reference}
+       * @see {@link https://en.wikipedia.org/wiki/Date_format_by_country#Table_coding|Wikipedia time coding reference}
+       */
+      date () {
+        const MONTHS = [
+          'January', 'February', 'March', 'April', 'May', 'June',
+          'July', 'August', 'September', 'October', 'November', 'December'
+        ]
+        const month = MONTHS[this.now.month]
+
+        return `${this.now.date} ${month}, ${this.now.year}`
+      }
+    },
+    methods: {
+      /**
+       * Get the current date and populate the data variables with the current
+       * day of the week, date, month and year.
+       */
+      setDate () {
+        const now = new Date()
+
+        this.now.dayOfWeek = now.getDay()
+
+        this.now.date = now.getDate()
+        this.now.month = now.getMonth()
+        this.now.year = now.getFullYear()
+      },
+
+      /**
+       * Start the looper that updates the day and date at fixed intervals. The
+       * function first stops the looper if already running.
+       */
+      startInterval () {
+        if (this.looper !== null) {
+          this.stopInterval()
+        }
+
+        this.setDate() // Set the date now
+        this.looper = setInterval(() => { // Set it to auto-update at regular intervals
+          this.setDate()
+        }, this.interval * 60 * 1000)
+      },
+      /**
+       * Stop the looper that updates the day and date at fixed intervals.
+       */
+      stopInterval () {
+        if (this.looper !== null) {
+          clearInterval(this.looper)
+        }
+        this.looper = null
+      }
+    },
+    created () {
+      this.startInterval()
+    },
+    beforeDestroy () {
+      this.stopInterval()
+    }
+  }
+</script>
