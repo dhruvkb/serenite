931e33c1b00d81249725ad725742dbde9cc46581
Dhruv Bhanushali <dhruv_b@live.com>
2020-11-21 20:17:21 +0530

    Create a component for displaying and choosing a color

---
+++ src/components/labels/color-picker/ColorPicker.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="color-picker">
+    <LabelSwatch
+      v-for="(colorCode, colorKey) in colorCodes"
+      :key="colorKey"
+      :color="colorCode"
+      :is-checked="colorKey === color"
+      :title="colorNames[colorKey]"
+      @click="handleClick(colorKey)"/>
+  </div>
+</template>
+
+<script>
+  import { Label } from '@/store/support/models'
+
+  import LabelSwatch from '@/components/labels/label-swatch/LabelSwatch'
+
+  export default {
+    name: 'ColorPicker',
+    components: {
+      LabelSwatch
+    },
+    model: {
+      prop: 'color',
+      event: 'pick'
+    },
+    props: {
+      /**
+       * the key of the chosen color
+       */
+      color: {
+        type: String
+      }
+    },
+    data () {
+      return {
+        colorNames: Label.COLOR_NAMES,
+        colorCodes: Label.COLOR_CODES
+      }
+    },
+    methods: {
+      handleClick (color) {
+        this.$emit('pick', color)
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss" src="./ColorPicker.scss"/>
---
+++ src/components/labels/label-swatch/LabelSwatch.vue
@@ -0,0 +1,76 @@
+<template>
+  <button
+    v-on="$listeners"
+    class="label-swatch"
+    :class="classes"
+    :style="styles">
+    <SereneIcon
+      class="box"
+      name="box"
+      :path="icons.box"/>
+  </button>
+</template>
+
+<script>
+  import SereneIcon from '@/atoms/serene-icon/SereneIcon'
+
+  import check from '@/assets/icons/check.svg'
+  import box from '@/assets/icons/box.svg'
+
+  export default {
+    name: 'LabelSwatch',
+    components: {
+      SereneIcon
+    },
+    data () {
+      return {
+        icons: {
+          check,
+          box
+        }
+      }
+    },
+    props: {
+      /**
+       * the color to present on the swatch
+       */
+      color: {
+        type: String,
+        required: true
+      },
+      /**
+       * whether the checkbox is checked
+       */
+      isChecked: {
+        type: Boolean,
+        default: false
+      }
+    },
+    computed: {
+      /**
+       * Get the classes to apply on the label swatch.
+       *
+       * @return {Array} an array of classes to apply on the element
+       */
+      classes () {
+        return [
+          {
+            'is-checked': this.isChecked
+          }
+        ]
+      },
+      /**
+       * Get the styles to apply on the label swatch.
+       *
+       * @return {Object} an object of styles to apply on the element
+       */
+      styles () {
+        return {
+          '--swatch-color': this.color
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss" src="./LabelSwatch.scss"/>
