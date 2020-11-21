3cb85eae108102cceb4fc17e6fb615535ff3eacd
Dhruv Bhanushali <dhruv_b@live.com>
2020-11-21 14:12:54 +0530

    Set icon in the icon slot

--- src/views/task-page/TaskPage.vue
+++ src/views/task-page/TaskPage.vue
@@ -1,22 +1,40 @@
 <template>
   <div class="task-page">
     <Today/>
-    <TaskInput/>
+    <TaskInput>
+      <template #icon>
+        <SereneIcon
+          name="rocket"
+          :path="icons.rocket"/>
+      </template>
+    </TaskInput>
     <TaskList/>
   </div>
 </template>
 
 <script>
+  import SereneIcon from '@/atoms/serene-icon/SereneIcon'
+
   import Today from '@/components/tasks/today/Today'
   import TaskList from '@/components/tasks/task-list/TaskList'
   import TaskInput from '@/components/tasks/task-input/TaskInput'
 
+  import rocket from '@/assets/icons/rocket.svg'
+
   export default {
     name: 'TaskPage',
     components: {
+      SereneIcon,
       Today,
       TaskInput,
       TaskList
+    },
+    data () {
+      return {
+        icons: {
+          rocket
+        }
+      }
     }
   }
 </script>
