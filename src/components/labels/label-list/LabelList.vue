478b663d1040339b923873eeae1ef76191cdcfec
Dhruv Bhanushali <dhruv_b@live.com>
2020-11-21 22:31:26 +0530

    Create a component to list labels along the lines of `TaskList`

---
+++ src/components/labels/label-list/LabelList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="label-list">
+    <transition
+      name="fade"
+      mode="out-in">
+      <div
+        v-if="allLabels.length"
+        key="list"
+        class="list">
+        <LabelUnit
+          v-for="label in allLabels"
+          :key="label.id"
+          :label="label"/>
+      </div>
+      <div
+        v-else
+        key="no-org"
+        class="no-org">
+        <p class="advice">
+          Use labels to organise your tasks.
+        </p>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+
+  import LabelUnit from '@/components/labels/label-unit/LabelUnit'
+
+  export default {
+    name: 'LabelList',
+    components: {
+      LabelUnit
+    },
+    computed: {
+      ...mapGetters('todo', [
+        'allLabels'
+      ])
+    }
+  }
+</script>
+
+<style scoped lang="scss" src="./LabelList.scss"/>
