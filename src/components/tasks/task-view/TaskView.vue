fb08aca8848004e421361e39d017934158a99294
Dhruv Bhanushali <dhruv_b@live.com>
2020-11-22 21:42:39 +0530

    Render inline labels in bold and in color

--- src/components/tasks/task-view/TaskView.vue
+++ src/components/tasks/task-view/TaskView.vue
@@ -2,6 +2,7 @@
   <SereneCard
     class="task-view"
     :class="classes"
+    :style="styles"
     :is-disabled="task.isComplete"
     @click="handleClick"
     @mouseenter="handleMouseEnter"
@@ -11,16 +12,23 @@
     @edit="handleEdit"
     @delete="handleDelete">
     <TaskCheckbox
+      :color="isLabelled ? label.colorKey : null"
       :is-focused="isMouseOver"
       :is-checked="task.isComplete"/>
     <span class="title">
-      {{ task.title }}
+      <span
+        v-for="(word, index) in task.title.split(' ')"
+        :key="index"
+        class="word"
+        :class="wordClasses(word)">
+        {{ word }}
+      </span>
     </span>
   </SereneCard>
 </template>
 
 <script>
-  import { mapActions } from 'vuex'
+  import { mapActions, mapGetters } from 'vuex'
 
   import { Task } from '@/store/support/models'
 
@@ -47,15 +55,69 @@
       }
     },
     computed: {
+      /**
+       * Get the classes to apply on the task view.
+       *
+       * @return {Array} an array of classes to apply on the element
+       */
       classes () {
         return [
           {
             'is-complete': this.task.isComplete
           }
         ]
-      }
+      },
+      /**
+       * Get the styles to apply on the task view.
+       *
+       * @return {Object} an object of styles to apply on the element
+       */
+      styles () {
+        if (this.isLabelled) {
+          return {
+            '--view-color': this.label.colorCode
+          }
+        } else {
+          return {}
+        }
+      },
+
+      /**
+       * Get whether this task has a label associated with it.
+       *
+       * @return {boolean} whether this task has an associated label
+       */
+      isLabelled () {
+        return this.label !== undefined
+      },
+
+      /**
+       * Get the `Label` instance associated with this task.
+       *
+       * @return {Label} the label associated with this task
+       */
+      label () {
+        return this.task.label(this.allLabels)
+      },
+
+      ...mapGetters('todo', [
+        'allLabels'
+      ])
     },
     methods: {
+      /**
+       * Get the classes to apply on every word in the task title.
+       *
+       * @return {Array} an array of classes to apply on the element
+       */
+      wordClasses (word) {
+        return [
+          {
+            'is-label': this.isLabelled && word === `@${this.label.name}`
+          }
+        ]
+      },
+
       /**
        * Set the new completion status of the task.
        */
@@ -85,7 +147,7 @@
       },
 
       handleClick () {
-        this.updateTaskIsComplete(!this.isComplete)
+        this.updateTaskIsComplete(!this.task.isComplete)
       },
       handleEdit () {
         this.$emit('edit')
@@ -106,3 +168,5 @@
     }
   }
 </script>
+
+<style scoped lang="scss" src="./TaskView.scss"/>
