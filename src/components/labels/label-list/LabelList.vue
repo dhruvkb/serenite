d546a595606f0cf4b472fba8e69d8aa04dce9315
Dhruv Bhanushali <dhruv_b@live.com>
2020-11-23 20:07:31 +0530

    Use the generic list view to enable drag and drop on both tasks and labels

--- src/components/labels/label-list/LabelList.vue
+++ src/components/labels/label-list/LabelList.vue
@@ -1,45 +1,75 @@
 <template>
-  <div class="label-list">
-    <transition
-      name="fade"
-      mode="out-in">
-      <div
-        v-if="allLabels.length"
-        key="list"
-        class="list">
-        <LabelUnit
-          v-for="label in allLabels"
-          :key="label.id"
-          :label="label"/>
-      </div>
-      <div
-        v-else
-        key="no-org"
-        class="no-org">
-        <p class="advice">
-          Use labels to organise your tasks.
-        </p>
-      </div>
-    </transition>
-  </div>
+  <SereneList
+    v-model="labelsWrapped"
+    class="label-list">
+    <template #default="props">
+      <LabelUnit
+        :key="props.item.id"
+        :label="props.item"/>
+    </template>
+    <template #no-list>
+      <p>Use labels to organise your tasks.</p>
+    </template>
+  </SereneList>
 </template>
 
 <script>
-  import { mapGetters } from 'vuex'
+  import { mapGetters, mapMutations } from 'vuex'
+
+  import SereneList from '@/atoms/serene-list/SereneList'
 
   import LabelUnit from '@/components/labels/label-unit/LabelUnit'
 
   export default {
     name: 'LabelList',
     components: {
+      SereneList,
+
       LabelUnit
     },
     computed: {
+      /**
+       * Get the list of all labels. This wrapper also enables `v-model` to
+       * update the orders of the labels via a Vuex mutation.
+       */
+      labelsWrapped: {
+        get () {
+          return this.allLabels
+        },
+        set (value) {
+          const orderedIds = value.map(label => label.id)
+          this.allLabels.forEach(label => {
+            const newOrder = orderedIds.indexOf(label.id)
+            if (label.order !== newOrder) {
+              this.updateLabelOrder(label.id, newOrder)
+            }
+          })
+        }
+      },
+
       ...mapGetters('todo', [
         'allLabels'
+      ])
+    },
+    methods: {
+      /**
+       * Set the new order of the label.
+       *
+       * @param {string} labelId - the ID of the label to reorder
+       * @param {number} labelOrder - the new order of the label
+       */
+      updateLabelOrder (labelId, labelOrder) {
+        this.editLabel({
+          labelAttrs: {
+            id: labelId,
+            order: labelOrder
+          }
+        })
+      },
+
+      ...mapMutations('todo', [
+        'editLabel'
       ])
     }
   }
 </script>
-
-<style scoped lang="scss" src="./LabelList.scss"/>
--- src/components/tasks/task-list/TaskList.vue
+++ src/components/tasks/task-list/TaskList.vue
@@ -1,48 +1,83 @@
 <template>
-  <div class="task-list">
-    <transition
-      name="fade"
-      mode="out-in">
-      <div
-        v-if="allTasks.length"
-        key="list"
-        class="list">
-        <TaskUnit
-          v-for="task in allTasks"
-          :key="task.id"
-          :task="task"/>
-      </div>
-      <div
-        v-else
-        key="day-off"
-        class="day-off">
-        <svg
-          class="balance"
-          viewBox="0 0 128 128"
-          xmlns="http://www.w3.org/2000/svg">
-          <use href="@/assets/balance.svg#balance"/>
-        </svg>
-        <p class="advice">
-          Take a deep breath and unwind.
-        </p>
-      </div>
-    </transition>
-  </div>
+  <SereneList
+    v-model="tasksWrapped"
+    class="task-list">
+    <template #default="props">
+      <TaskUnit
+        :key="props.item.id"
+        :task="props.item"/>
+    </template>
+    <template #no-list>
+      <svg
+        class="balance"
+        viewBox="0 0 128 128"
+        xmlns="http://www.w3.org/2000/svg">
+        <use href="@/assets/balance.svg#balance"/>
+      </svg>
+      <p>Take a deep breath and unwind.</p>
+    </template>
+  </SereneList>
 </template>
 
 <script>
-  import { mapGetters } from 'vuex'
+  import { mapActions, mapGetters } from 'vuex'
+
+  import SereneList from '@/atoms/serene-list/SereneList'
 
   import TaskUnit from '@/components/tasks/task-unit/TaskUnit'
 
   export default {
     name: 'TaskList',
     components: {
+      SereneList,
+
       TaskUnit
     },
     computed: {
+      /**
+       * Get the list of all tasks. This wrapper also enables `v-model` to
+       * update the orders of the tasks via a Vuex mutation.
+       */
+      tasksWrapped: {
+        get () {
+          return this.allTasks
+        },
+        set (value) {
+          const orderedIds = value.map(task => task.id)
+          this.allTasks.forEach(task => {
+            const newOrder = orderedIds.indexOf(task.id)
+            if (task.order !== newOrder) {
+              this.updateTaskOrder(task.id, newOrder)
+            }
+          })
+        }
+      },
+
       ...mapGetters('todo', [
         'allTasks'
+      ])
+    },
+    methods: {
+      /**
+       * Set the new order of the task.
+       *
+       * @param {string} taskId - the ID of the task to reorder
+       * @param {number} taskOrder - the new order of the task
+       */
+      updateTaskOrder (taskId, taskOrder) {
+        this.updateTasks({
+          mutation: 'editTask',
+          data: {
+            taskAttrs: {
+              id: taskId,
+              order: taskOrder
+            }
+          }
+        })
+      },
+
+      ...mapActions('todo', [
+        'updateTasks'
       ])
     }
   }
